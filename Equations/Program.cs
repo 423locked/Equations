7d6190ad4cf7bb07631dc955f076df0a9fd8617b
Yaroslav Prishedko <yaroslav05042005@gmail.com>
2021-11-05 12:28:54 +0300

    Добавьте файлы проекта.

---
+++ Equations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Solver;
+
+namespace Equations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = QuadraticEquationsSolver.Solve(1, -3, 2);
+
+            Console.WriteLine($"x1={solutions.Item1}, x2={solutions.Item2}");
+        }
+    }
+}
---
+++ Solver/QuadraticEquationsSolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solver
+{
+    public class QuadraticEquationsSolver
+    {
+        public static Tuple<double, double> Solve(double a, double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+            return Tuple.Create(x1, x2);
+        }
+    }
+}
